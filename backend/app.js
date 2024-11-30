669de31b74fee69628771a9544efedd55b90ba41
ABUBAKARKHAN-STACK <abubakar123456654123@gmail.com>
2024-11-30 18:22:59 +0500

    changing

--- backend/app.js
+++ backend/app.js
@@ -4,6 +4,7 @@ import cors from "cors";
 import "dotenv/config";
 import { connectDB } from "./config/index.js";
 import helmet from "helmet";
+import {ApiResponse , ApiError} from "./utils/index.js";
 
 
 const app = express();
@@ -36,8 +37,8 @@ app.use(
             "style-src": ["'self'", "'unsafe-inline'"],
         },
     })
-);    
-  
+);
+
 app.use(static_("public"))
 
 app.get("/", (req, res) => {
@@ -46,7 +47,7 @@ app.get("/", (req, res) => {
         message: "API is working"
     })
 })
-  
+
 // User Routes
 import userRoutes from './routes/user.routes.js'
 app.use("/users", userRoutes)
@@ -55,8 +56,41 @@ app.use("/users", userRoutes)
 import courseRoutes from './routes/course.routes.js'
 app.use("/courses", courseRoutes)
 
+import { authUrl, oauth2Client } from './middlewares/auth2.0.middleware.js';
+
+app.get("/auth", (req, res) => {
+    // Redirect to Google OAuth consent page
+    res.redirect(authUrl); // This should take the user to Google's consent screen
+});
+
+app.get("/auth/callback", async (req, res) => {
+    const { code } = req.query;
+    
+    if (!code) {
+        return res.status(400).json({ error: 'Authorization code is missing' });
+    }
+
+    console.log('Redirected to /auth/callback with code:', code);  // Log the code
+
+    try {
+        const { tokens } = await oauth2Client.getToken(code);
+        oauth2Client.setCredentials(tokens);
+        console.log('Access Token:', tokens.access_token);
+        console.log('Refresh Token:', tokens.refresh_token);
+        res.json({ message: 'Authentication successful', tokens });
+    } catch (error) {
+        console.error('Error while exchanging token:', error);
+        res.status(500).json({ error: 'Failed to exchange token' });
+    }
+});
+
+
+
+
+
 // Video Routes
 import videoRoutes from './routes/video.routes.js'
+
 app.use("/videos", videoRoutes)
 
 
