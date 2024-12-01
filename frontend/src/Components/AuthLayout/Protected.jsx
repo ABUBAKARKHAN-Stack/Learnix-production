850f982e9bc424614cdf0c459e8cdf192b7a5722
ABUBAKARKHAN-STACK <abubakar123456654123@gmail.com>
2024-12-01 18:35:31 +0500

    fixing

--- frontend/src/Components/AuthLayout/Protected.jsx
+++ frontend/src/Components/AuthLayout/Protected.jsx
@@ -9,6 +9,7 @@ function Protected({ children, authenticationRequired = true }) {
     const navigate = useNavigate()
 
     useEffect(() => {
+        console.log(token, authenticationRequired);
         if (!token && authenticationRequired) {
 
             navigate('/signin')
